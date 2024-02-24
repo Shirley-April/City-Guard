6b52fb0c0bfdf98c0acc5dd76d77024353c82892
Shirley-April <shirleyapril23@gmail.com>
2024-02-24 13:53:34 +0300

    add trash enemy and destroy upon collision

--- src/GameSceneTwo/index.js
+++ src/GameSceneTwo/index.js
@@ -1,4 +1,12 @@
 export default class GameSceneTwo extends Phaser.Scene {
+  player;
+  cursors;
+
+  trash1;
+  trash2;
+
+  score;
+
   constructor() {
     super({ key: "GameSceneTwo" });
   }
@@ -8,9 +16,31 @@ export default class GameSceneTwo extends Phaser.Scene {
       "player",
       "https://content.codecademy.com/courses/learn-phaser/physics/codey.png"
     );
+
+    this.load.image("trash1", "assets/trash1.png");
+    this.load.image("trash2", "assets/trash2.png");
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
+
   create() {
-    this.add.image(200, 300, "player");
+    this.player = this.physics.add.sprite(200, 300, "player");
+    this.trash1 = this.physics.add.image(300, 300, "trash1");
+    this.trash2 = this.physics.add.image(400, 300, "trash2");
+
+    this.physics.add.collider(this.player, this.trash1, () => {
+      this.trash1.destroy();
+    });
+
+    this.physics.add.collider(this.player, this.trash2, () => {
+      this.trash2.destroy();
+    });
   }
-  update() {}
+  update() {
+    if (this.cursors.right.isDown) {
+      this.player.setVelocityX(100);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  }
 }
