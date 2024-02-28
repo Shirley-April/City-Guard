6471051ee84aaf3e31fc9e976219b8c49d20da9d
Shirley-April <shirleyapril23@gmail.com>
2024-02-28 12:34:20 +0300

    add all game scenes

--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ import GameSceneOne from "./GameSceneOne/index.js";
 import GameSceneTwo from "./GameSceneTwo/index.js";
 import GameSceneThree from "./GameSceneThree/index.js";
 import GameSceneTravel from "./GameSceneTravel/index.js";
+import GameSceneFour from "./GameSceneFour/index.js";
 
 let config = {
   type: Phaser.AUTO,
   width: 1200,
   height: 620,
   backgroundColor: "b9eaff",
-  scene: [GameSceneThree, GameSceneTravel, GameSceneOne, GameSceneTwo],
+  scene: [
+    GameSceneOne,
+    GameSceneTwo,
+    GameSceneThree,
+    GameSceneTravel,
+    GameSceneFour,
+  ],
   physics: {
     default: "arcade",
     arcade: {
