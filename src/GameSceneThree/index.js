634e7c83b3a6a8304aec144817e1b272e85beb15
Shirley-April <shirleyapril23@gmail.com>
2024-02-28 11:04:40 +0300

    add automobile selection scene

--- src/GameSceneThree/index.js
+++ src/GameSceneThree/index.js
@@ -1,5 +1,11 @@
+function startSceneTravel(props) {
+  setTimeout(() => {
+    props.scene.stop("GameSceneThree");
+    props.scene.start("GameSceneFour");
+  }, 2000);
+}
 export default class GameSceneThree extends Phaser.Scene {
-  player;
+  player1;
 
   cursors;
 
@@ -7,48 +13,67 @@ export default class GameSceneThree extends Phaser.Scene {
   cybertruck;
   vehicle;
 
+  keyB;
+  keyC;
+  keyN;
+
   constructor() {
     super({ key: "GameSceneThree" });
   }
 
   preload() {
+    this.load.image("city", "assets/city-view.jpg");
     this.load.image("electricbus", "assets/electricbus.png");
     this.load.image("cybertruck", "assets/cybertruck.png");
     this.load.image("vehicle", "assets/fuelvehicle.png");
-
-    this.load.image(
-      "player",
-      "https://content.codecademy.com/courses/learn-phaser/physics/codey.png"
-    );
+    this.load.image("selectionPopUp", "assets/selectionText.PNG");
+    this.load.image("player1", "assets/man.png");
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
   create() {
-    this.player = this.physics.add.image(450, 300, "player");
+    const height = this.scale.height;
 
-    this.electricbus = this.physics.add
-      .image(500, 150, "electricbus")
-      .setScale(0.5);
-    this.cybertruck = this.physics.add
-      .image(500, 250, "cybertruck")
-      .setScale(0.4);
-    this.vehicle = this.physics.add.image(500, 350, "vehicle").setScale(0.5);
+    this.city = this.add.image(600, height, "city").setOrigin(0, 1);
+    this.electricbus = this.physics.add.image(400, 500, "electricbus");
+    this.cybertruck = this.physics.add.image(900, 500, "cybertruck");
 
-    this.physics.add.collider(this.player, this.electricbus, () => {
-      this.scene.stop("GameSceneThree");
-      this.scene.start("GameSceneTravel");
-    });
+    this.vehicle = this.physics.add.image(1400, 500, "vehicle");
+
+    this.player1 = this.physics.add.image(100, 500, "player1").setScale(0.3);
+    this.selectionPopUp = this.physics.add
+      .image(300, 100, "selectionPopUp")
+      .setScale(1);
+
+    this.keyB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
+    this.keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+    this.keyN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
+
+    this.cameras.main.startFollow(this.player1);
+    this.cameras.main.setBounds(0, 0, 2500, height);
   }
   update() {
     if (this.cursors.right.isDown) {
-      this.player.setVelocityX(100);
+      this.player1.setVelocityX(400);
+    } else if (this.cursors.left.isDown) {
+      this.player1.setVelocityX(-400);
     } else if (this.cursors.up.isDown) {
-      this.player.setVelocityY(-100);
+      this.player1.setVelocityY(-100);
     } else if (this.cursors.down.isDown) {
-      this.player.setVelocityY(100);
+      this.player1.setVelocityY(100);
     } else {
-      this.player.setVelocityY(0);
-      this.player.setVelocityX(0);
+      this.player1.setVelocityY(0);
+      this.player1.setVelocityX(0);
+    }
+
+    if (this.keyB.isDown) {
+      startSceneTravel(this);
+    }
+    if (this.keyC.isDown) {
+      startSceneTravel(this);
+    }
+    if (this.keyN.isDown) {
+      startSceneTravel(this);
     }
   }
 }
